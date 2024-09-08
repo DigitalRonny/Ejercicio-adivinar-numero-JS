2144ed4113d3130e0973ef99abaf1358cdd8621a
Ronny Cartagena <rondamian04@gmail.com>
2024-09-08 17:46:40 -0500

    feat(Ronny): comprobar la jugabilidad en consola

--- logica.js
+++ logica.js
@@ -1,7 +1,20 @@
-let random = Math.floor(Math.random() * 10);
+let numSecreto = Math.floor(Math.random() * 10);
 let intentos = 10
 
 
 function adivinarNumero(){
     let obtenerNumero = document.getElementById("miNumero").value
+
+    do {
+        if (obtenerNumero == numSecreto) {
+            console.log(`Ganaste!, el numero secreto era ${numSecreto}`)
+            break
+        }
+        if (obtenerNumero < numSecreto) {
+            console.log("El numero secreto es mas grande")
+        }
+        if (obtenerNumero > numSecreto) {
+            console.log("El numero secreto es mas bajo")
+        }
+    } while (numSecreto != obtenerNumero && --intentos > 0);
 }
