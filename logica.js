73b4e595785ee1c8837dff13dfa8c47e355de1c1
Ronny Cartagena <rondamian04@gmail.com>
2024-09-08 18:16:10 -0500

    feat(Ronny): correccion de la logica por ifs

--- logica.js
+++ logica.js
@@ -5,16 +5,26 @@ let intentos = 10
 function adivinarNumero(){
     let obtenerNumero = document.getElementById("miNumero").value
 
-    do {
+    //1 verificar si aún quedan intentos
+    if (intentos > 0) {
         if (obtenerNumero == numSecreto) {
             console.log(`Ganaste!, el numero secreto era ${numSecreto}`)
-            break
+            intentos = 0; //termina el juego
+            return; //salimos para que no se sigan reduciendo los intentos
         }
         if (obtenerNumero < numSecreto) {
-            console.log("El numero secreto es mas grande")
+                console.log("El numero secreto es mas grande")
         }
         if (obtenerNumero > numSecreto) {
             console.log("El numero secreto es mas bajo")
         }
-    } while (numSecreto != obtenerNumero && --intentos > 0);
+        //2 reducir el numero de intentos
+        intentos--
+        console.log(`Te quedan ${intentos} intentos`)
+    }
+    
+    //3 verificar si se acabaron los intentos
+    if (intentos == 0 && numSecreto != obtenerNumero ) {
+        console.log(`Lo siento se acabaron tus intentos, el numero secreto era ${numSecreto}`)
+    }
 }
